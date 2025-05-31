63a30daa7db1f3505ab90996672a753c1126faf9
hobbyist-programmer-ar <hobbyistprogrammer@outlook.com>
2025-05-31 09:07:28 +0530

    35. Search Insert Position

--- src/main/java/org/hobbiesofar/binarysearch/InsertPosition.java
+++ src/main/java/org/hobbiesofar/binarysearch/InsertPosition.java
@@ -2,7 +2,6 @@ package org.hobbiesofar.binarysearch;
 
 class InsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int n = nums.length;
         int left = 0;
         int right = nums.length;
         while(left < right) {
